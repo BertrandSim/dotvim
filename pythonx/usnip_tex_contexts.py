4823d355de17e95a34ea1d7f96602db67a2dcf3d
Bertrand Sim <bertrand.sim@users.noreply.github.com>
2020-12-09 17:32:56 +0800

    Simplify tex math* context
    Simplified math() and math_vimtex_2() to a single math() context
    Use regex matching for texMathZone syntax names
    instead of actual texMathZone+[A-Z] names
    Taken from a pr by Konfekt to gillescastel's latex snippets repo

--- pythonx/usnip_tex_contexts.py
+++ pythonx/usnip_tex_contexts.py
@@ -9,56 +9,36 @@
 import vim
 
 # math blocks
+# simplified as per https://github.com/gillescastel/latex-snippets/pull/14/files
+# and compatible with vimtex 2.0 syntax
 
-texMathZones = ['texMathZone'+x for x in ['A', 'AS', 'B', 'BS', 'C', 'CS', 'D', 'DS',       \
-    'E', 'ES', 'F', 'FS', 'G', 'GS', 'H', 'HS', 'I', 'IS', 'J', 'JS', 'K', 'KS', 'L', 'LS', \
-    'V', 'W', 'X', 'Y', 'Z']]
-
-texIgnoreMathZones = ['texMathText']
-
-texMathZoneIds = vim.eval('map('+str(texMathZones)+", 'hlID(v:val)')")
-texIgnoreMathZoneIds = vim.eval('map('+str(texIgnoreMathZones)+", 'hlID(v:val)')")
+texMathZones = ['texMathZone']
+texTextZones = ['texMathText']
+texZones     = texMathZones + texTextZones
 
 def math():
-	synstackids = vim.eval("synstack(line('.'), col('.') - (col('.')>=2 ? 1 : 0))")
-	try:
-		first = next(i for i in reversed(synstackids) if i in texIgnoreMathZoneIds or i in texMathZoneIds)
-		# return first != texIgnoreMathZoneIds[0]
-		return first not in texIgnoreMathZoneIds
-	except StopIteration:
-		return False
-
-
-# math blocks, with vimtex 2.0 plugin
-
-texMathRegions = ['texMathRegion'+x for x in ['', 'X', 'XX', 'Env', 'EnvStarred', 'Ensured']]
-texIgnoreMathRegions = ['texMathTextArg']
-
-texMathRegionIds = vim.eval('map('+str(texMathRegions)+", 'hlID(v:val)')")
-texIgnoreMathRegionIds = vim.eval('map('+str(texIgnoreMathRegions)+", 'hlID(v:val)')")
-
-def math_vimtex_2():
-	synstackids = vim.eval("synstack(line('.'), col('.') - (col('.')>=2 ? 1 : 0))")
-	try:
-		first = next(i for i in reversed(synstackids) if i in texIgnoreMathRegionIds or i in texMathRegionIds)
-		# return first != texIgnoreMathRegionIds[0]
-		return first not in texIgnoreMathRegionIds
-	except StopIteration:
-		return False
-
-
+    synstackNames = vim.eval("map(synstack(line('.'), max([col('.') - 1, 1])), 'synIDattr(v:val, ''name'')')")
+    try:
+        first = next(
+            i for i in reversed(synstackNames)
+            if any(i.find(z) == 0 for z in texZones)
+        )
+        return any(first.find(z) == 0 for z in texMathZones)
+    except StopIteration:
+        return False
 
 
 # numbered (labelled) equation environments
+# modified from Gilles Castel's original code for math context 
 
 texEqZones = ['texMathZoneC', 'texMathZoneE']
              # equation env ,  align env
 texEqZoneIds = vim.eval('map('+str(texEqZones)+", 'hlID(v:val)')")
 
 def eq_env():
-	synstackids = vim.eval("synstack(line('.'), col('.') - (col('.')>=2 ? 1 : 0))")
-	try:
-		first = next(i for i in reversed(synstackids) if i in texEqZoneIds)
-		return True
-	except StopIteration:
-		return False 
+    synstackids = vim.eval("synstack(line('.'), col('.') - (col('.')>=2 ? 1 : 0))")
+    try:
+        first = next(i for i in reversed(synstackids) if i in texEqZoneIds)
+        return True
+    except StopIteration:
+        return False 
