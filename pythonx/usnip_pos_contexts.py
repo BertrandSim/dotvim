a7e8573572525dcb585057772626427743accbba
Bertrand Sim <bertrand.sim@users.noreply.github.com>
2021-03-18 13:47:30 +0800

    Ultisnips: remove unnecessary import vim in usnip_pos_contexts.py

--- pythonx/usnip_pos_contexts.py
+++ pythonx/usnip_pos_contexts.py
@@ -1,8 +1,6 @@
 # Ultisnip contexts for file positions
 # see :h UltiSnips-custom-context-snippets
 
-import vim
-
 # at top `num_lines` of file?
 def top_of_file(snip, num_lines = 1):
     return snip.line < num_lines
